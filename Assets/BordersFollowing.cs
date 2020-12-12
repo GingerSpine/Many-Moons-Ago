cebc026ed646daba5d384ea16d2e69ef13f1037f
Vladimir Strizhakov <v.stryzhakov@enestech.net>
2020-12-12 19:27:31 +0200

    Camera attached to the player. Drawing trajectory of jumping.

---
+++ Assets/BordersFollowing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BordersFollowing : MonoBehaviour
+{
+    public Transform cameraPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraPosition.position.y > transform.position.y)
+        {
+            Vector3 newPosition = new Vector3(transform.position.x, cameraPosition.position.y, transform.position.z);
+            transform.position = newPosition;
+        }
+    }
+}
--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -2,7 +2,6 @@
 
 public class CameraFollowScript : MonoBehaviour
 {
-
     public Transform cameraPosition;
     public float smoothmoveSpeed = 0.3f;
     public float allowedOffset = 0.3f;
@@ -16,10 +15,7 @@ public class CameraFollowScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (cameraPosition.position.y > transform.position.y + allowedOffset || cameraPosition.position.y < transform.position.y - allowedOffset)
-        {
-            Vector3 newPosition = new Vector3(cameraPosition.position.x, cameraPosition.position.y, transform.position.z);
-            transform.position = newPosition;
-        }
+        Vector3 newPosition = new Vector3(cameraPosition.position.x, cameraPosition.position.y, transform.position.z);
+        transform.position = newPosition;
     }
 }
