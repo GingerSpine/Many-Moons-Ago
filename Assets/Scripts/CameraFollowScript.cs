a4b7594c0ca06ba4cd18c94a6716664fa671489e
Vladimir Strizhakov <v.stryzhakov@enestech.net>
2020-12-12 17:45:25 +0200

    Camera following logic changed

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -1,23 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraFollowScript : MonoBehaviour {
+public class CameraFollowScript : MonoBehaviour
+{
 
     public Transform cameraPosition;
-    public float smoothmoveSpeed =0.3f;
+    public float smoothmoveSpeed = 0.3f;
+    public float allowedOffset = 0.3f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-        if(cameraPosition.position.y > transform.position.y)
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (cameraPosition.position.y > transform.position.y + allowedOffset || cameraPosition.position.y < transform.position.y - allowedOffset)
         {
-            Vector3 newPosition = new Vector3(transform.position.x, cameraPosition.position.y,transform.position.z);
+            Vector3 newPosition = new Vector3(transform.position.x, cameraPosition.position.y, transform.position.z);
             transform.position = newPosition;
         }
     }
