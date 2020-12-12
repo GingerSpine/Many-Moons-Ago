a0a0a16e1f8455ca861ad6a2344294e9cb92e534
Maksym_Terentyev <teren.maxim@gmail.com>
2020-12-12 22:49:33 +0200

    Finish Intro

---
+++ Assets/Scripts/MainMenu/IntroBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroBehaviour : MonoBehaviour
+{
+    public float timeBeforeDiying;
+    void Update()
+    {
+        if (timeBeforeDiying <= 0)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        else
+        {
+            timeBeforeDiying -= Time.deltaTime;
+        }
+    }
+}
