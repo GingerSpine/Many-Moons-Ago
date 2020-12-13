20fbd0d9ba420062885ae272bd679d96c64e86b2
MasterD20 <voroninihor@gmail.com>
2020-12-13 15:31:47 +0200

    55

---
+++ Assets/OnCollisionDestroySelf.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionDestroySelf : MonoBehaviour
+{
+    public GameObject GameObject;
+    void OnCollisionStart2D(Collision2D other)
+    {
+        if (GameObject.tag == other.gameObject.tag)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator waiter()
+    {
+        //Wait for 4 seconds
+        yield return new WaitForSeconds(4);
+        Destroy(gameObject);
+    }
+}
+
