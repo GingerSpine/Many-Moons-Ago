68e21148eb65f125a429457e6d23319310d63570
MasterD20 <voroninihor@gmail.com>
2020-12-13 10:21:13 +0200

    new: platform

--- Assets/Scripts/asteroid/AsteroidCollision.cs
+++ Assets/Scripts/asteroid/AsteroidCollision.cs
@@ -6,6 +6,7 @@ public class AsteroidCollision : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameObject GameObject;
+    public GameObject platform;
 
     void OnCollisionStay2D(Collision2D other)
     {
@@ -13,6 +14,8 @@ public class AsteroidCollision : MonoBehaviour
         //Debug.Log("Collision"); 
         if (rb.gameObject.tag == other.gameObject.tag && rb.velocity.y <= 0)
         {
+            var oth_pos = other.gameObject.transform.position;
+            Instantiate(platform, new Vector3(oth_pos.x+0.2f, oth_pos.y-0.6f, 0f), Quaternion.identity);
             var _explodable = GetComponent<Explodable>();
             _explodable.explode();
             ExplosionForce ef = GameObject.FindObjectOfType<ExplosionForce>();
